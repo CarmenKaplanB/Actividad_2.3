5de38867fdd57e670102860592b0d93bc2f905c9
Carmen Kaplan <viajero19976@innovaccion.mx>
2021-10-17 18:26:41 +0000

    Final commit

--- code/training.py
+++ code/training.py
@@ -2,7 +2,7 @@
 
 import sklearn
 import pandas as pd
-from sklearn.model_selection import train_test_split#.Dividir.los.datos.en.train.y.test
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
